4c4d9040a2fd4be6767ea055376286edfa0ce898
George Ankoma-Mensah <k.paks.n@gmail.com>
2026-02-09 18:08:03 +0000

    Add files via upload
    The first 2 pages according to the document (subject to change)
    Merely a skeleton

---
+++ About.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>Marshfield School</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <div class="grid-container">
+
+    <div class="header">
+      <h1>Marshfield School History</h1>
+    </div>
+
+    <div class="picture">
+      
+    </div>
+
+    <nav class="navbar">
+      <div class="navbuttons">
+        <ul>
+          <li><a href="About.php">About</a></li>
+          <li><a href="">Chapel Green Board School</a></li>
+          <li><a href="">Thorton Lane Board School</a></li>
+          <li><a href="">Marshfield School</a></li>
+          <li class="align"><a href="Staff db.xlsx">Staff database</a></li>
+          <li><a href="Sources db.xlsx">Sources database</a></li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="content-grid">
+      <div class="subPage">
+        <header>
+          <h3>About</h3>
+        </header>
+
+        <p>
+          This application is a Community Research project focusing on the history of Marshfield School (Little Horton District) which is part of Bradfords educational history. It is intended as a resource for anyone researching Bradfords educational history or just interested in the history of Marshfield School.
+          It should be noted that all the information available in this application was provided under a personal, research and educational copy write. Therefore, it is likely to be covered by some other form of other copy-right restrictions. See copy-write statement at the end of this text. If you ignore this warning 
+          on copy-right infringement’s, then as the person carrying out the infringement you will be solely responsible for any repercussions.
+          The information available to users shows the school through the lens of School Logs Books, Admission Books, Punishment Books, Images and other sources. Using these  sources enables the school history to be viewed from its conception as Chapel Green Board School through to the present Marshfield School.
+          Some of the resources included here have been collected and published (though not widely) by Mr D Craven in 2004. Many thanks the D Craven for allowing me to merge my research with that of his own.
+          Major resources like the School Logbooks offer a unique window into the past from which general information on the day to day running of the school can be seen. From these comments many social aspects, of the time, can be seen and occasionally the head teacher’s private thoughts or frustrations are expressed. 
+          Admission books show when a person entered the school, their fathers name, Standards attained and when the left school. Once again, another source full of social and societal information just waiting to be analysed.
+          Noteable dates are intended to show key dates for researchers / interested parties to note.
+          Two booklets that have been published about Marshfield School are included in the sources. The first is the ‘Historical Sketch’ of Marshfield School written by J Jackson. The second is ‘Marshfield School: Our heroes of World War 1’ written by Ray Greenhough. To read the two booklets click <a href="Page_1a.php">‘here’</a>
+        </p>
+      </div>
+
+      <div class="noteableDates">
+        <header>
+          <h3>Noteable Dates</h3>
+        </header>
+
+        <p>
+          1870: Education Act enables local councils to open, operate and fund elementary education through the public purse.<br>
+          1870: The purpose of the Act was to provide Education where there were insufficient Church School provision. Often called “Filling the Gaps”.<br>
+          1870: Bradford set up its School Board in November.<br>
+          1873: The School Board opened its first schools in rented buildings.<br>
+          1874: Eight School Board schools were built and opened.<br>
+          1876: Parents become responsible for ensuring their children learnt the 3 r’s.<br>
+          1876: Bradford School Board created an attendance committee.<br>
+          1876: Restricted child labour by linking it to passing certain school standards.<br>
+        </p>
+      </div>
+    </div>
+
+    <div class="footer">
+    <p>Community History Research by Ray Greenhough</p>
+    </div>
+  </div>
+</body>
+</html>
---
+++ Page_1a.php
@@ -0,0 +1,86 @@
+<html>
+  <head>
+     <title>Marshfield School</title>
+     <link rel="stylesheet" href="Page-css.css"/>
+</head>
+<body>
+         <nav class="navbar">
+      <div class="navbuttons">
+        <ul>
+          <li><a href="About.php">About</a></li>
+          <li><a href="chapelgreen.html">Chapel Green Board School</a></li>
+          <li><a href="thortonlane.html">Thorton Lane Board School</a></li>
+          <li><a href="marshfield.html">Marshfield School</a></li>
+          <li class="align"><a href="Staff db.xlsx">Staff database</a></li>
+          <li><a href="Sources db.xlsx">Sources database</a></li>
+        </ul>
+      </div>
+    </nav>
+
+    <div>
+     <h3>A Historical Sketch</h3>
+</div>
+    
+    <div class="ahs-grid">
+      <a href="">Front cover</a>
+      <a href="">Page 1</a>
+      <a href="">Page 2</a>
+      <a href="">Page 3</a>
+      <a href="">Page 4</a>
+      <a href="">Page 5</a>
+      <a href="">Page 6</a>
+      <a href="">Page 7</a>
+      <a href="">Page 8</a>
+      <a href="">Page 9</a>
+      <a href="">Page 10</a>
+      <a href="">Page 11</a>
+      <a href="">Page 12</a>
+      <a href="">Page 13</a>
+      <a href="">Page 14</a>
+      <a href="">Page 15</a>
+      <a href="">Page 16</a>
+      <a href="">Page 17</a>
+      <a href="">Page 18</a>
+      <a href="">Page 19</a>
+      <a href="">Page 20</a>
+      <a href="">Page 21</a>
+      <a href="">Page 22</a>
+      <a href="">Page 23</a>
+      <a href="">Page 24</a>
+      <a href="">Page 25</a>
+      <a href="">Page 26</a>
+      <a href="">Page 27</a>
+      <a href="">Page 28</a>
+</div>
+
+ &nbsp;
+
+  <nav class="navbar">
+      <div class="navbuttons">
+        <ul>
+          <li><a href="About.php">About</a></li>
+          <li><a href="chapelgreen.html">Chapel Green Board School</a></li>
+          <li><a href="thortonlane.html">Thorton Lane Board School</a></li>
+          <li><a href="marshfield.html">Marshfield School</a></li>
+          <li class="align"><a href="Staff db.xlsx">Staff database</a></li>
+          <li><a href="Sources db.xlsx">Sources database</a></li>
+        </ul>
+      </div>
+    </nav>
+
+    <div>
+      <h3>
+        Mashfield School<br>
+        Our Hero's of WW1  
+      </h3>
+
+</div>
+     <div class="ohw-grid">
+       <?php
+       for ($i = 1; $i <= 80; $i++) {
+      echo "<div class='grid-item'>Page $i</div>";
+  }
+?>
+</div>
+</body>
+</html>
