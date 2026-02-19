a75287a0797d63b24bec124730fd345bc057adb2
Estherareba <162845740+Estherareba@users.noreply.github.com>
2026-02-19 10:41:42 +0000

    Add files via upload

--- sources.sql
+++ sources.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 19, 2026 at 11:33 AM
+-- Generation Time: Feb 19, 2026 at 11:37 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,48 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `sources`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `department`
---
-
-CREATE TABLE `department` (
-  `department_id` int(11) NOT NULL,
-  `department_name` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `department`
---
-
-INSERT INTO `department` (`department_id`, `department_name`) VALUES
-(1, 'Infant'),
-(2, 'Junior'),
-(3, 'Senior');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `school`
---
-
-CREATE TABLE `school` (
-  `school_id` int(11) NOT NULL,
-  `school_name` varchar(150) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `school`
---
-
-INSERT INTO `school` (`school_id`, `school_name`) VALUES
-(4, 'Bradford School Board'),
-(1, 'Chapel Green Board School'),
-(5, 'Chapel Green Church School'),
-(3, 'Marshfield School'),
-(2, 'Thornton Lane Board School');
 
 -- --------------------------------------------------------
 
@@ -239,63 +197,9 @@ INSERT INTO `source` (`source_id`, `title`, `school_id`, `department_id`, `sourc
 (151, 'Plan of Chapel Green Board School', 1, NULL, 4, NULL, NULL, NULL, NULL),
 (152, 'Plan of Chapel Green Church School', 5, NULL, 4, NULL, NULL, NULL, NULL);
 
--- --------------------------------------------------------
-
---
--- Table structure for table `source_pages`
---
-
-CREATE TABLE `source_pages` (
-  `page_id` int(11) NOT NULL,
-  `source_id` int(11) NOT NULL,
-  `page_number` int(11) DEFAULT NULL,
-  `page_label` varchar(50) DEFAULT NULL,
-  `school_id` int(11) DEFAULT NULL,
-  `notes` varchar(255) DEFAULT NULL,
-  `hyperlink` varchar(500) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `source_type`
---
-
-CREATE TABLE `source_type` (
-  `source_type_id` int(11) NOT NULL,
-  `type_name` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `source_type`
---
-
-INSERT INTO `source_type` (`source_type_id`, `type_name`) VALUES
-(2, 'Admission Register'),
-(7, 'Doc'),
-(3, 'Images'),
-(1, 'Log Book'),
-(4, 'Plan'),
-(6, 'Punishment Book'),
-(5, 'School Board');
-
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `department`
---
-ALTER TABLE `department`
-  ADD PRIMARY KEY (`department_id`),
-  ADD UNIQUE KEY `department_name` (`department_name`);
-
---
--- Indexes for table `school`
---
-ALTER TABLE `school`
-  ADD PRIMARY KEY (`school_id`),
-  ADD UNIQUE KEY `school_name` (`school_name`);
 
 --
 -- Indexes for table `source`
@@ -307,53 +211,14 @@ ALTER TABLE `source`
   ADD KEY `source_type_id` (`source_type_id`);
 
 --
--- Indexes for table `source_pages`
---
-ALTER TABLE `source_pages`
-  ADD PRIMARY KEY (`page_id`),
-  ADD KEY `source_id` (`source_id`),
-  ADD KEY `school_id` (`school_id`);
-
---
--- Indexes for table `source_type`
---
-ALTER TABLE `source_type`
-  ADD PRIMARY KEY (`source_type_id`),
-  ADD UNIQUE KEY `type_name` (`type_name`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
-
---
--- AUTO_INCREMENT for table `department`
---
-ALTER TABLE `department`
-  MODIFY `department_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `school`
---
-ALTER TABLE `school`
-  MODIFY `school_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `source`
 --
 ALTER TABLE `source`
   MODIFY `source_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=153;
-
---
--- AUTO_INCREMENT for table `source_pages`
---
-ALTER TABLE `source_pages`
-  MODIFY `page_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `source_type`
---
-ALTER TABLE `source_type`
-  MODIFY `source_type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Constraints for dumped tables
@@ -366,13 +231,6 @@ ALTER TABLE `source`
   ADD CONSTRAINT `source_ibfk_1` FOREIGN KEY (`school_id`) REFERENCES `school` (`school_id`),
   ADD CONSTRAINT `source_ibfk_2` FOREIGN KEY (`department_id`) REFERENCES `department` (`department_id`),
   ADD CONSTRAINT `source_ibfk_3` FOREIGN KEY (`source_type_id`) REFERENCES `source_type` (`source_type_id`);
-
---
--- Constraints for table `source_pages`
---
-ALTER TABLE `source_pages`
-  ADD CONSTRAINT `source_pages_ibfk_1` FOREIGN KEY (`source_id`) REFERENCES `source` (`source_id`),
-  ADD CONSTRAINT `source_pages_ibfk_2` FOREIGN KEY (`school_id`) REFERENCES `school` (`school_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
