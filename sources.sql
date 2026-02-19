10c557470422ca5218e19015ed05976f6c188923
Estherareba <162845740+Estherareba@users.noreply.github.com>
2026-02-19 11:16:11 +0000

    Add files via upload
    latest sql file as at today

--- sources.sql
+++ sources.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 19, 2026 at 11:37 AM
+-- Generation Time: Feb 19, 2026 at 12:13 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -18,8 +18,50 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `sources`
+-- Database: `sources2`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `department`
+--
+
+CREATE TABLE `department` (
+  `department_id` int(11) NOT NULL,
+  `department_name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `department`
+--
+
+INSERT INTO `department` (`department_id`, `department_name`) VALUES
+(1, 'Infant'),
+(2, 'Junior'),
+(3, 'Senior');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `school`
+--
+
+CREATE TABLE `school` (
+  `school_id` int(11) NOT NULL,
+  `school_name` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `school`
+--
+
+INSERT INTO `school` (`school_id`, `school_name`) VALUES
+(4, 'Bradford School Board'),
+(1, 'Chapel Green Board School'),
+(5, 'Chapel Green Church School'),
+(3, 'Marshfield School'),
+(2, 'Thornton Lane Board School');
 
 -- --------------------------------------------------------
 
@@ -81,125 +123,65 @@ INSERT INTO `source` (`source_id`, `title`, `school_id`, `department_id`, `sourc
 (35, 'Thornton Lane School opening', 2, NULL, 7, NULL, NULL, NULL, NULL),
 (36, 'Extracts form Bradford School Board Minutes 1877-78', 4, NULL, 5, NULL, NULL, 1877, 1878),
 (37, 'Plan of Chapel Green Board School', 1, NULL, 4, NULL, NULL, NULL, NULL),
-(38, 'Plan of Chapel Green Church School', 5, NULL, 4, NULL, NULL, NULL, NULL),
-(39, 'Infant School Log Book 1877 - 1905', 1, 1, 1, 'Not available', NULL, 1877, 1905),
-(40, 'Infant School Log Book 1877 - 1905', 2, 1, 1, 'Not available', NULL, 1877, 1905),
-(41, 'Infant School Log Book 1905 - 1939', NULL, 1, 1, 'Not available', NULL, 1905, 1939),
-(42, 'Infant School Log Book 1905 - 1939', NULL, 1, 1, 'Not available', NULL, 1905, 1939),
-(43, 'Infant School Log Book 1939 - 1969', 3, 1, 1, NULL, NULL, 1939, 1969),
-(44, 'Infant School Log Book 1969 - 1973', 3, 1, 1, NULL, NULL, 1969, 1973),
-(45, 'Infant Admission Register Sep 1943 - Jan 1954', 3, 1, 2, NULL, NULL, 1943, 1954),
-(46, 'Infant Admission Register Sep 1954 - Jul 1965', 3, 1, 2, NULL, NULL, 1954, 1965),
-(47, 'Infant Admission Register Sep 1965 - Jan 1973', 3, 1, 2, NULL, NULL, 1965, 1973),
-(48, 'Photos - May day celebrations', 3, 1, 3, NULL, NULL, NULL, NULL),
-(49, 'Junior School Log Book 1877 - 1910', 1, 2, 1, 'Not available', NULL, 1877, 1910),
-(50, 'Junior School Log Book 1877 - 1910', 2, 2, 1, 'Not available', NULL, 1877, 1910),
-(51, 'Junior School Log Book 1910 - 1933', NULL, 2, 1, 'Not available', NULL, 1910, 1933),
-(52, 'Junior School Log Book 1933 - 1964', 3, 2, 1, NULL, NULL, 1933, 1964),
-(53, 'Junior Admission Register Jan 1894 - Feb 1901', 2, 2, 2, NULL, NULL, 1894, 1901),
-(54, 'Junior Admission Register Apr 1906 - Sep 1911', 3, 2, 2, NULL, NULL, 1906, 1911),
-(55, 'Junior Admission Register Aug 1911 - Jul 1917', 3, 2, 2, NULL, NULL, 1911, 1917),
-(56, 'Senior Admission Register Jul 1901 - Jul 1928', 3, 3, 2, NULL, NULL, 1901, 1928),
-(57, 'Junior Admission Register Jan Aug 1928 - Jan 1944', 3, 2, 2, NULL, NULL, 1928, 1944),
-(58, 'Junior Admission Register Mar 1944 - Feb 1954', 3, 2, 2, NULL, NULL, 1944, 1954),
-(59, 'Junior Admission Register Mar 1954 - Jan 1963', 3, 2, 2, NULL, NULL, 1954, 1963),
-(60, 'Junior Admission Register Sep 1971 - Sep 1973', 3, 2, 2, NULL, NULL, 1971, 1973),
-(61, 'Punishment book 1902 - 1965', 3, 2, 6, NULL, NULL, 1902, 1965),
-(62, 'Miss Teal\'s scapbook 1931 - 1952', 3, 2, 3, NULL, NULL, 1931, 1952),
-(63, 'Miss Teal\'s scrapbook tour to Northern Ireland 1949', 3, 2, 3, NULL, NULL, 1949, 1949),
-(64, 'Miss Teal\'s scrapbook tour to London 1950 and Bournemouth 1851', 3, 2, 3, NULL, NULL, 1950, 1951),
-(65, 'Photograph album circa 1955-56', 3, 2, 3, NULL, NULL, 1955, 1956),
-(66, 'Miss Leach/Mrs Emmerson\'s album', 3, 2, 3, NULL, NULL, NULL, NULL),
-(67, 'Miscellaneous pictures', 3, 2, 3, NULL, NULL, NULL, NULL),
-(68, 'Marshfield School Magazine 1926', 3, 2, 3, NULL, NULL, 1926, 1926),
-(69, 'Extracts from Marshfield Infant Log book 1905 - 1936', 3, 2, 3, NULL, NULL, 1905, 1936),
-(70, 'Proposed plan of Junior and Senior Mixed School', 3, 2, 4, NULL, NULL, NULL, NULL),
-(71, 'Plan for new Infant School', 3, 1, 4, NULL, NULL, NULL, NULL),
-(72, 'Plan for new Thornton Lane Board School', 2, NULL, 4, NULL, NULL, NULL, NULL),
-(73, 'Thornton Lane School opening', 2, NULL, 7, NULL, NULL, NULL, NULL),
-(74, 'Extracts form Bradford School Board Minutes 1877-78', 4, NULL, 5, NULL, NULL, 1877, 1878),
-(75, 'Plan of Chapel Green Board School', 1, NULL, 4, NULL, NULL, NULL, NULL),
-(76, 'Plan of Chapel Green Church School', 5, NULL, 4, NULL, NULL, NULL, NULL),
-(77, 'Infant School Log Book 1877 - 1905', 1, 1, 1, 'Not available', NULL, 1877, 1905),
-(78, 'Infant School Log Book 1877 - 1905', 2, 1, 1, 'Not available', NULL, 1877, 1905),
-(79, 'Infant School Log Book 1905 - 1939', NULL, 1, 1, 'Not available', NULL, 1905, 1939),
-(80, 'Infant School Log Book 1905 - 1939', NULL, 1, 1, 'Not available', NULL, 1905, 1939),
-(81, 'Infant School Log Book 1939 - 1969', 3, 1, 1, NULL, NULL, 1939, 1969),
-(82, 'Infant School Log Book 1969 - 1973', 3, 1, 1, NULL, NULL, 1969, 1973),
-(83, 'Infant Admission Register Sep 1943 - Jan 1954', 3, 1, 2, NULL, NULL, 1943, 1954),
-(84, 'Infant Admission Register Sep 1954 - Jul 1965', 3, 1, 2, NULL, NULL, 1954, 1965),
-(85, 'Infant Admission Register Sep 1965 - Jan 1973', 3, 1, 2, NULL, NULL, 1965, 1973),
-(86, 'Photos - May day celebrations', 3, 1, 3, NULL, NULL, NULL, NULL),
-(87, 'Junior School Log Book 1877 - 1910', 1, 2, 1, 'Not available', NULL, 1877, 1910),
-(88, 'Junior School Log Book 1877 - 1910', 2, 2, 1, 'Not available', NULL, 1877, 1910),
-(89, 'Junior School Log Book 1910 - 1933', NULL, 2, 1, 'Not available', NULL, 1910, 1933),
-(90, 'Junior School Log Book 1933 - 1964', 3, 2, 1, NULL, NULL, 1933, 1964),
-(91, 'Junior Admission Register Jan 1894 - Feb 1901', 2, 2, 2, NULL, NULL, 1894, 1901),
-(92, 'Junior Admission Register Apr 1906 - Sep 1911', 3, 2, 2, NULL, NULL, 1906, 1911),
-(93, 'Junior Admission Register Aug 1911 - Jul 1917', 3, 2, 2, NULL, NULL, 1911, 1917),
-(94, 'Senior Admission Register Jul 1901 - Jul 1928', 3, 3, 2, NULL, NULL, 1901, 1928),
-(95, 'Junior Admission Register Jan Aug 1928 - Jan 1944', 3, 2, 2, NULL, NULL, 1928, 1944),
-(96, 'Junior Admission Register Mar 1944 - Feb 1954', 3, 2, 2, NULL, NULL, 1944, 1954),
-(97, 'Junior Admission Register Mar 1954 - Jan 1963', 3, 2, 2, NULL, NULL, 1954, 1963),
-(98, 'Junior Admission Register Sep 1971 - Sep 1973', 3, 2, 2, NULL, NULL, 1971, 1973),
-(99, 'Punishment book 1902 - 1965', 3, 2, 6, NULL, NULL, 1902, 1965),
-(100, 'Miss Teal\'s scapbook 1931 - 1952', 3, 2, 3, NULL, NULL, 1931, 1952),
-(101, 'Miss Teal\'s scrapbook tour to Northern Ireland 1949', 3, 2, 3, NULL, NULL, 1949, 1949),
-(102, 'Miss Teal\'s scrapbook tour to London 1950 and Bournemouth 1851', 3, 2, 3, NULL, NULL, 1950, 1951),
-(103, 'Photograph album circa 1955-56', 3, 2, 3, NULL, NULL, 1955, 1956),
-(104, 'Miss Leach/Mrs Emmerson\'s album', 3, 2, 3, NULL, NULL, NULL, NULL),
-(105, 'Miscellaneous pictures', 3, 2, 3, NULL, NULL, NULL, NULL),
-(106, 'Marshfield School Magazine 1926', 3, 2, 3, NULL, NULL, 1926, 1926),
-(107, 'Extracts from Marshfield Infant Log book 1905 - 1936', 3, 2, 3, NULL, NULL, 1905, 1936),
-(108, 'Proposed plan of Junior and Senior Mixed School', 3, 2, 4, NULL, NULL, NULL, NULL),
-(109, 'Plan for new Infant School', 3, 1, 4, NULL, NULL, NULL, NULL),
-(110, 'Plan for new Thornton Lane Board School', 2, NULL, 4, NULL, NULL, NULL, NULL),
-(111, 'Thornton Lane School opening', 2, NULL, 7, NULL, NULL, NULL, NULL),
-(112, 'Extracts form Bradford School Board Minutes 1877-78', 4, NULL, 5, NULL, NULL, 1877, 1878),
-(113, 'Plan of Chapel Green Board School', 1, NULL, 4, NULL, NULL, NULL, NULL),
-(114, 'Plan of Chapel Green Church School', 5, NULL, 4, NULL, NULL, NULL, NULL),
-(115, 'Infant School Log Book 1877 - 1905', 1, 1, 1, 'Not available', NULL, 1877, 1905),
-(116, 'Infant School Log Book 1877 - 1905', 2, 1, 1, 'Not available', NULL, 1877, 1905),
-(117, 'Infant School Log Book 1905 - 1939', NULL, 1, 1, 'Not available', NULL, 1905, 1939),
-(118, 'Infant School Log Book 1905 - 1939', NULL, 1, 1, 'Not available', NULL, 1905, 1939),
-(119, 'Infant School Log Book 1939 - 1969', 3, 1, 1, NULL, NULL, 1939, 1969),
-(120, 'Infant School Log Book 1969 - 1973', 3, 1, 1, NULL, NULL, 1969, 1973),
-(121, 'Infant Admission Register Sep 1943 - Jan 1954', 3, 1, 2, NULL, NULL, 1943, 1954),
-(122, 'Infant Admission Register Sep 1954 - Jul 1965', 3, 1, 2, NULL, NULL, 1954, 1965),
-(123, 'Infant Admission Register Sep 1965 - Jan 1973', 3, 1, 2, NULL, NULL, 1965, 1973),
-(124, 'Photos - May day celebrations', 3, 1, 3, NULL, NULL, NULL, NULL),
-(125, 'Junior School Log Book 1877 - 1910', 1, 2, 1, 'Not available', NULL, 1877, 1910),
-(126, 'Junior School Log Book 1877 - 1910', 2, 2, 1, 'Not available', NULL, 1877, 1910),
-(127, 'Junior School Log Book 1910 - 1933', NULL, 2, 1, 'Not available', NULL, 1910, 1933),
-(128, 'Junior School Log Book 1933 - 1964', 3, 2, 1, NULL, NULL, 1933, 1964),
-(129, 'Junior Admission Register Jan 1894 - Feb 1901', 2, 2, 2, NULL, NULL, 1894, 1901),
-(130, 'Junior Admission Register Apr 1906 - Sep 1911', 3, 2, 2, NULL, NULL, 1906, 1911),
-(131, 'Junior Admission Register Aug 1911 - Jul 1917', 3, 2, 2, NULL, NULL, 1911, 1917),
-(132, 'Senior Admission Register Jul 1901 - Jul 1928', 3, 3, 2, NULL, NULL, 1901, 1928),
-(133, 'Junior Admission Register Jan Aug 1928 - Jan 1944', 3, 2, 2, NULL, NULL, 1928, 1944),
-(134, 'Junior Admission Register Mar 1944 - Feb 1954', 3, 2, 2, NULL, NULL, 1944, 1954),
-(135, 'Junior Admission Register Mar 1954 - Jan 1963', 3, 2, 2, NULL, NULL, 1954, 1963),
-(136, 'Junior Admission Register Sep 1971 - Sep 1973', 3, 2, 2, NULL, NULL, 1971, 1973),
-(137, 'Punishment book 1902 - 1965', 3, 2, 6, NULL, NULL, 1902, 1965),
-(138, 'Miss Teal\'s scapbook 1931 - 1952', 3, 2, 3, NULL, NULL, 1931, 1952),
-(139, 'Miss Teal\'s scrapbook tour to Northern Ireland 1949', 3, 2, 3, NULL, NULL, 1949, 1949),
-(140, 'Miss Teal\'s scrapbook tour to London 1950 and Bournemouth 1851', 3, 2, 3, NULL, NULL, 1950, 1951),
-(141, 'Photograph album circa 1955-56', 3, 2, 3, NULL, NULL, 1955, 1956),
-(142, 'Miss Leach/Mrs Emmerson\'s album', 3, 2, 3, NULL, NULL, NULL, NULL),
-(143, 'Miscellaneous pictures', 3, 2, 3, NULL, NULL, NULL, NULL),
-(144, 'Marshfield School Magazine 1926', 3, 2, 3, NULL, NULL, 1926, 1926),
-(145, 'Extracts from Marshfield Infant Log book 1905 - 1936', 3, 2, 3, NULL, NULL, 1905, 1936),
-(146, 'Proposed plan of Junior and Senior Mixed School', 3, 2, 4, NULL, NULL, NULL, NULL),
-(147, 'Plan for new Infant School', 3, 1, 4, NULL, NULL, NULL, NULL),
-(148, 'Plan for new Thornton Lane Board School', 2, NULL, 4, NULL, NULL, NULL, NULL),
-(149, 'Thornton Lane School opening', 2, NULL, 7, NULL, NULL, NULL, NULL),
-(150, 'Extracts form Bradford School Board Minutes 1877-78', 4, NULL, 5, NULL, NULL, 1877, 1878),
-(151, 'Plan of Chapel Green Board School', 1, NULL, 4, NULL, NULL, NULL, NULL),
-(152, 'Plan of Chapel Green Church School', 5, NULL, 4, NULL, NULL, NULL, NULL);
+(38, 'Plan of Chapel Green Church School', 5, NULL, 4, NULL, NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `source_pages`
+--
+
+CREATE TABLE `source_pages` (
+  `page_id` int(11) NOT NULL,
+  `source_id` int(11) NOT NULL,
+  `page_number` int(11) DEFAULT NULL,
+  `page_label` varchar(50) DEFAULT NULL,
+  `school_id` int(11) DEFAULT NULL,
+  `notes` varchar(255) DEFAULT NULL,
+  `hyperlink` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `source_type`
+--
+
+CREATE TABLE `source_type` (
+  `source_type_id` int(11) NOT NULL,
+  `type_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `source_type`
+--
+
+INSERT INTO `source_type` (`source_type_id`, `type_name`) VALUES
+(2, 'Admission Register'),
+(7, 'Doc'),
+(3, 'Images'),
+(1, 'Log Book'),
+(4, 'Plan'),
+(6, 'Punishment Book'),
+(5, 'School Board');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `department`
+--
+ALTER TABLE `department`
+  ADD PRIMARY KEY (`department_id`),
+  ADD UNIQUE KEY `department_name` (`department_name`);
+
+--
+-- Indexes for table `school`
+--
+ALTER TABLE `school`
+  ADD PRIMARY KEY (`school_id`),
+  ADD UNIQUE KEY `school_name` (`school_name`);
 
 --
 -- Indexes for table `source`
@@ -211,14 +193,53 @@ ALTER TABLE `source`
   ADD KEY `source_type_id` (`source_type_id`);
 
 --
+-- Indexes for table `source_pages`
+--
+ALTER TABLE `source_pages`
+  ADD PRIMARY KEY (`page_id`),
+  ADD KEY `source_id` (`source_id`),
+  ADD KEY `school_id` (`school_id`);
+
+--
+-- Indexes for table `source_type`
+--
+ALTER TABLE `source_type`
+  ADD PRIMARY KEY (`source_type_id`),
+  ADD UNIQUE KEY `type_name` (`type_name`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `department`
+--
+ALTER TABLE `department`
+  MODIFY `department_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `school`
+--
+ALTER TABLE `school`
+  MODIFY `school_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `source`
 --
 ALTER TABLE `source`
-  MODIFY `source_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=153;
+  MODIFY `source_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+
+--
+-- AUTO_INCREMENT for table `source_pages`
+--
+ALTER TABLE `source_pages`
+  MODIFY `page_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `source_type`
+--
+ALTER TABLE `source_type`
+  MODIFY `source_type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Constraints for dumped tables
@@ -231,6 +252,13 @@ ALTER TABLE `source`
   ADD CONSTRAINT `source_ibfk_1` FOREIGN KEY (`school_id`) REFERENCES `school` (`school_id`),
   ADD CONSTRAINT `source_ibfk_2` FOREIGN KEY (`department_id`) REFERENCES `department` (`department_id`),
   ADD CONSTRAINT `source_ibfk_3` FOREIGN KEY (`source_type_id`) REFERENCES `source_type` (`source_type_id`);
+
+--
+-- Constraints for table `source_pages`
+--
+ALTER TABLE `source_pages`
+  ADD CONSTRAINT `source_pages_ibfk_1` FOREIGN KEY (`source_id`) REFERENCES `source` (`source_id`),
+  ADD CONSTRAINT `source_pages_ibfk_2` FOREIGN KEY (`school_id`) REFERENCES `school` (`school_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
